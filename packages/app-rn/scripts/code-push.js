a003962472cb46ca0d9ae50df5746f7ce3401c0f
OF3808 <hannianqi@qianmi.com>
2022-10-08 19:34:15 +0800

    chore: add codepush ci

---
+++ packages/app-rn/scripts/code-push.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+const execa = require('execa');
+const path = require('path');
+const { appName, extraArgs } = require('minimist')(process.argv.slice(2));
+
+if (appName) {
+  throw new Error(`Arg appName missing !`);
+}
+
+execa.sync(`appcenter release-react -a '${appName}' -d Production ${extraArgs || ''}`, {
+  stdio: 'inherit',
+  cwd: path.join(__dirname, '..'),
+  env: {
+    APPCENTER_ACCESS_TOKEN: process.env.APPCENTER_ACCESS_TOKEN
+  }
+});
